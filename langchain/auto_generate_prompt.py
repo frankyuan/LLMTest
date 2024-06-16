5f56ac5103d2138993d86c650313a2447d6e9786
Frank Yuan <yuan_zhi_fei@hotmail.com>
2024-06-16 20:06:28 +0800

    fix: improve logic

--- langchain/auto_generate_prompt.py
+++ langchain/auto_generate_prompt.py
@@ -1,101 +1,162 @@
+import os
 from typing import TypedDict, Callable
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
-from langgraph.graph import END, MessageGraph
+from langgraph.graph import END, StateGraph
 
 MAX_TRY = 10 # Just try 10 times to generate prompt
 
 class AgentState(TypedDict):
-    instruction: str
-    data_source: list
-    target_result: str
+    request: str
+    user_input: str
+    expect_output: str
+    test_cases: list
     current_prompt: str
+    prompt_history: list
     best_prompt: str
     best_score: float
     actual_result: str
     current_score: float
     try_times: int
-    open_source_executor: Callable[[str], str]
+    target_LLM: Callable[[str], str]
 
 
-def init_agent_state(instruction, data_source, target_result, open_source_executor=None):
+def init_agent_state(request, user_input, expect_output, test_cases, target_LLM):
     agent_state = AgentState(
-        instruction=instruction, 
-        data_source=data_source, 
-        target_result=target_result,
+        request=request, 
+        user_input=user_input, 
+        expect_output=expect_output,
+        test_cases=test_cases,
         current_prompt="",
+        prompt_history=[],
         best_prompt="",
         best_score = 0,
         actual_result="",
         current_score = 0,
         try_times=0,
-        open_source_executor=open_source_executor)
+        target_LLM=target_LLM)
     return agent_state
 
 
-def generate_prompt(state):
-    prompt = get_generate_prompt_prompt(state)
+def generate_prompt(agent_state):
+    prompt = get_prompt_generation_prompt(agent_state)
     model = get_model_by_name()
-    result = model.invoke(HumanMessage(content=prompt))
-    state.try_times += 1
-    state.current_prompt = result  # TODO: need to extract prompt from result
+    result = model.invoke(prompt)
+    content = result.content
+    agent_state["try_times"] += 1
+    agent_state["current_prompt"] = content
+    agent_state["prompt_history"].append(content)
+    return agent_state
 
 
-def invoke_open_source_model(state):
-    state.actual_result = state.open_source_executor(state.current_prompt)
+def evaluate_result_of_prompt(agent_state):
+    target_LLM = agent_state["target_LLM"]
+    test_cases = agent_state["test_cases"]
+    current_prompt_template = agent_state["current_prompt"]
+    passed_count = 0
+    total_count = len(test_cases)
+    for test_case in test_cases:
+        current_prompt = current_prompt_template.replace("{user_input}", test_case["user_input"])
+        result = target_LLM.invoke(current_prompt)
+        prompt = get_evaluate_result_prompt(result, test_case["expect_output"])
+        model = get_model_by_name() # TODO: maybe can use another LLM
+        evaluate_result = model.invoke(prompt)
+        evaluate_content = evaluate_result.content
+        if 'True' in evaluate_content or 'true' in evaluate_content:
+            passed_count += 1
+
+    score = passed_count/total_count
+    agent_state["current_score"] = score
+    if score > agent_state["best_score"]:
+        agent_state["best_score"] = score
+        agent_state["best_prompt"] = current_prompt_template
+    
+    return agent_state
 
 
-def evaluate_result_of_prompt(state):
-    prompt = get_evaluate_prompt_prompt(state)
-    model = get_model_by_name()
-    result = model.invoke(HumanMessage(content=prompt))
-    state.current_score = result.score
-    state.current_prompt = result.prompt
-    if result.score > state.best_score:
-        state.best_score = result.score
-        state.best_prompt = state.current_prompt
-
-
-def can_finish_to_generate_prompt(state):
-    if state.try_times >= MAX_TRY:
+def can_finish_to_generate_prompt(agent_state):
+    if agent_state["try_times"] >= MAX_TRY:
         return "true"
     
-    if state.current_score >= 0.9:
+    if agent_state["current_score"] >= 0.9:
         return "true"
     
     return "false"
 
 
 def execute_langgraph(agent_state):
-    graph = MessageGraph()
+    runnable = generate_graph()
+    result = runnable.invoke(agent_state)
+    return result
+
+
+def generate_graph():
+    graph = StateGraph(AgentState)
     graph.add_node("generate_prompt", generate_prompt)
-    graph.add_node("invoke_open_source_model", invoke_open_source_model)
     graph.add_node("evaluate_result_of_prompt", evaluate_result_of_prompt)
 
-    graph.add_edge("generate_prompt", "invoke_open_source_model")
-    graph.add_edge("invoke_open_source_model", "evaluate_result_of_prompt")
+    graph.add_edge("generate_prompt", "evaluate_result_of_prompt")
     graph.add_conditional_edges("evaluate_result_of_prompt", can_finish_to_generate_prompt, {
         "true": END, "false": "generate_prompt"
     })
 
     graph.set_entry_point("generate_prompt")
     runnable = graph.compile()
-    result = runnable.invoke(agent_state)
-    return result
+    return runnable
 
 
 def get_model_by_name(model_name="gpt-3.5-turbo"):
-    model = ChatOpenAI(temperature=0, model_name=model_name)
+    model = ChatOpenAI(temperature=0, model_name=model_name, openai_api_key=os.getenv("OPENAI_API_KEY"))
     return model
 
 
 def get_result_from_state(agent_state):
-    return agent_state.best_prompt, agent_state.best_score
+    return agent_state["best_prompt"], agent_state["best_score"]
 
 
-def get_generate_prompt_prompt(agent_state):
-    pass
+def get_prompt_generation_prompt(agent_state: AgentState):
+    request = agent_state["request"]
+    user_input = agent_state["user_input"]
+    expect_output = agent_state["expect_output"]
+    return f"""
+    你是一个用来生成prompt的AI，可以根据`用户的需求`和`用户的输入`生成能够返回`预期输出`的prompt。
+    `用户的输入`和`预期输出`都是用于帮助生成prompt的示例，和真正的输入和预期输出不同。
+
+    返回给用户的prompt的逻辑要清楚。对于复杂的需求，可以使用COT的方式生成prompt。
+    返回给用户的prompt 必须符合下面的规则：
+    - 必须包含角色定义
+    - 必须包含用户的输入。用户的输入用 {{user_input}} 这个格式表示，供后面的步骤用实际的数值替换。不能使用下面`用户的输入`，因为它只是用来帮助你生成prompt，不是用户实际的输入。
+    - 不能包含预期的输出。因为预期的输出是根据prompt来生成的。
+    - 直接返回生成的prompt
+
+    #### 用户的需求:
+    {request}
+    ####
+
+    #### 用户的输入:
+    {user_input}
+    ####
+
+    #### 预期输出:
+    {expect_output}
+    ####
+
+    在返回给用户前，检查是否达到下面的目标：
+    - 生成的prompt符合每一条`用户的需求`
+    - 生成的prompt必须能够返回`预期输出`
+    - 生成的prompt必须包含类似`用户的输入`: {{user_input}}`的格式。
+    - 直接返回生成的prompt，不需要返回任何其他信息，因为prompt会直接被作为后面的步骤给大模型的prompt。
+    - prompt是文本，不是JSON对象，也不能包含代码。
+    """
 
 
-def get_evaluate_prompt_prompt(agent_state):
-    pass
+def get_evaluate_result_prompt(result, expect_output):
+    return f"""
+    你是一个用来评估结果的AI，可以检查`结果`是否符合`预期的输出`，并只能返回['True', 'False']数组中一个值。符合预期则返回True，不符合预期则返回False。
+
+    结果:
+    {result}
+
+    预期的输出:
+    {expect_output}
+    """
--- langchain/auto_generate_prompt_test.py
+++ langchain/auto_generate_prompt_test.py
@@ -1,19 +1,30 @@
-from auto_generate_prompt import AgentState, execute_langgraph, init_agent_state, get_result_from_state
+from dotenv import load_dotenv
+
+from auto_generate_prompt import execute_langgraph, init_agent_state, get_result_from_state
+from langchain_community.llms import Tongyi
+
+load_dotenv("default.env")
 
 # user's request
-instruction = ""
-data_source = [{}]
-target_result = ""
+request = f"""
+- 找出下面文字中的时间
+- 用JSON返回, 只有一个字段'Key'
+"""
+user_input = "如果从2021年初以来持续三年多的调整来看，A股基本是一个负回报的市场。但如果将时间拉长到2018年初，尽管经历了大幅波动，但持有好股票的投资者依然取得了不俗的成绩。"
+expect_output = "[{'Key': '2021'},{'Key': '2018'}]"
+
+test_cases = [{'user_input': '在2018年至今的6年时间里，A股仅有两年的幸福时光，却有四年惨淡的调整。上证指数2018年初为3400点，当前上证指数仅有3000点。', 'expect_output': "[{'Key': '2018'}]"}]
 
 
-def call_open_source_model(prompt):
-    pass
+def get_target_model():
+    return Tongyi()
 
 
 agent_state = init_agent_state(
-    instruction=instruction, 
-    data_source=data_source, 
-    target_result=target_result,
-    open_source_executor=call_open_source_model)
+    request=request,
+    user_input=user_input, 
+    expect_output=expect_output,
+    test_cases=test_cases,
+    target_LLM=get_target_model())
 result_agent_state = execute_langgraph(agent_state)
 print(get_result_from_state(result_agent_state))
